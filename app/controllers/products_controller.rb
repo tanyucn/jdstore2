9db92456f4bdd6ff0bb5cdc446b56443ad5336f7
谭宇 <tanyucn@outlook.com>
2017-08-09 21:56:23 +0800

    test CRUD

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,39 @@
 class ProductsController < ApplicationController
-   def index
-     @products = Product.all
-   end
+def index
+  @groups = Group.all
+end
 
-  def show
-    @product = Product.find(params[:id])
+def new
+  @group = Group.new
+end
+
+def show
+@group = Group.find(params[:id])
+end
+
+def edit
+  @group = Group.find(params[:id])
+end
+
+def create
+  @group = Group.new(group_params)
+  if @group.save
+    redirect_to groups_path
+  else
+    render :new
   end
+end
 
+def update
+  @group = Group.find(params[:id])
+  if @group.update(group_params)
+    redirect_to groups_path
+  else
+    render :edit
+  end
+end
 
+def destroy
+  @group = Group.find(params[:id])
+end
 end
