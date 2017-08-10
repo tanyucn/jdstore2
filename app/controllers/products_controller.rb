4ba38e970f31bd9cd3b6cdc831c4e29ccb04c2c7
谭宇 <tanyucn@outlook.com>
2017-08-11 00:46:37 +0800

    fix

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,46 +1,9 @@
 class ProductsController < ApplicationController
-def index
-  @groups = Group.all
-end
-
-def new
-  @group = Group.new
-end
-
-def show
-@group = Group.find(params[:id])
-end
-
-def edit
-  @group = Group.find(params[:id])
-end
-
-def create
-  @group = Group.new(group_params)
-  if @group.save
-    redirect_to groups_path
-  else
-    render :new
+  def index
+    @products = Product.all
   end
-end
 
-def update
-  @group = Group.find(params[:id])
-  if @group.update(group_params)
-    redirect_to groups_path
-  else
-    render :edit
+  def show
+    @product = Product.find(params[:id])
   end
-end
-
-def destroy
-  @group = Group.find(params[:id])
-  @group.destroy
-  redirect_to grous_paht
-
-private
-
-def group_params
-  params.require(:group).permit(:title, :description)
-end
 end
